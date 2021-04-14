976e276d2e5d413195400f2c29ce49884cb0d223
Foxar <mostek3pl@gmail.com>
2021-04-14 13:16:47 +0200

    Created a controller for the caption model.

---
+++ VargSearchAPI/app/Http/Controllers/CaptionController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Caption;
+use Illuminate\Http\Request;
+
+class CaptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Caption  $caption
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Caption $caption)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Caption  $caption
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Caption $caption)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Caption  $caption
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Caption $caption)
+    {
+        //
+    }
+}
