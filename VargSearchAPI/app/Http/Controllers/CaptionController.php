2c654036ec7b8dbb61b273cd42367d09a3d98244
Foxar <mostek3pl@gmail.com>
2021-04-14 13:26:28 +0200

    Removed base controller class.

--- VargSearchAPI/app/Http/Controllers/CaptionController.php
+++ VargSearchAPI/app/Http/Controllers/CaptionController.php
@@ -5,60 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Caption;
 use Illuminate\Http\Request;
 
-class CaptionController extends Controller
+class CaptionController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+ 
+    //Return captions that match the search parameters.
+    public function show(Request $request)
     {
-        //
+        
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Caption  $caption
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Caption $caption)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Caption  $caption
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Caption $caption)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Caption  $caption
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Caption $caption)
-    {
-        //
-    }
 }
