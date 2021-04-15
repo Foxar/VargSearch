427fc49924ebb776d44cb5b1f6bd10b6329ac72e
Foxar <mostek3pl@gmail.com>
2021-04-15 11:59:54 +0200

    Modified Elqouent query to allow searching by value of 'manual' column.

--- VargSearchAPI/app/Http/Controllers/CaptionController.php
+++ VargSearchAPI/app/Http/Controllers/CaptionController.php
@@ -12,7 +12,10 @@ class CaptionController
     public function show(Request $request)
     {
         $query = $request->input('query');
-        $captions = Caption::where('text', 'like', '%'.$query.'%')->take(10)->get();
+        $captions = Caption::where('text', 'like', '%'.$query.'%')
+                ->where('manual', $request->gen)
+                ->take(10)
+                ->get();
         return $captions;
     }
 
